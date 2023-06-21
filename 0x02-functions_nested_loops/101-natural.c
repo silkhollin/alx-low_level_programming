c40a68ddb01e33438bfe7a8173657c8fda2df9fe
silkhollin <silcollinz@gmail.com>
2023-06-21 07:33:14 -0700

    printing all natural numbers that are multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 
-/*
- * main -  for printing all numbers less than 1024
- * must be multiples of 3 and 5
- * Return: 0 nga bulijjo
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
