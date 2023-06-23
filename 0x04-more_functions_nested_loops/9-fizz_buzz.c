4aefdae6834d9c0a241755db88143ed4166805fa
silkhollin <silcollinz@gmail.com>
2023-06-22 23:31:53 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,3 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_square(2);
-    print_square(10);
-    print_square(0);
-    return (0);
-}
-
-
-9-fizz_buzz.c
-
 #include "main.h"
 #include <stdio.h>
 
