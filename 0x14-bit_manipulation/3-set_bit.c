658127d4f0c05159e2750de1b5c61486ceda0262
silkhollin <silcollinz@gmail.com>
2023-08-03 12:35:48 -0700

    more bit operations

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > 63)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
+	*n  = ((1UL << index) | *n);
 
 	return (1);
 
