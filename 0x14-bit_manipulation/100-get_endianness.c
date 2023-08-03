0158c983c182e4b01103cd6b8c989a024beba41e
silkhollin <silcollinz@gmail.com>
2023-08-03 12:53:37 -0700

    more bit operations

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,14 +2,17 @@
 
 /**
  * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
+ * Return: 0 for big endian, 1 for little endian
  */
 
 int get_endianness(void)
 {
-	unsigned int num = 1;
-	char *c = (char *) & num;
+	unsigned int kol;
 
-	return (*c);
+	char *c;
+
+	kol = 1;
+	c = (char *) &kol;
+	return ((int)*c);
 }
 
