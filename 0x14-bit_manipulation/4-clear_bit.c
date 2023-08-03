bdacb87c5022293952e51275c24219198af18a15
silkhollin <silcollinz@gmail.com>
2023-08-03 12:27:22 -0700

    more bit operations

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	unsigned long int mask = ~(1UL << index);
