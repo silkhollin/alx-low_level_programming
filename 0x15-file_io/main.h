16fe84ddf96a0e89751f03d6382831d8695ef8f0
silkhollin <silcollinz@gmail.com>
2023-09-05 03:37:19 -0700

    final

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
